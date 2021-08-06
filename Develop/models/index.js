9db7dc3ca029fc8dbcda626a87a6398f9f2dc541
janayAl <jsallen298705@gmail.com>
2021-08-05 23:21:51 -0500

    fixed index models with foreigh keys

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,49 +7,64 @@ const { belongsTo } = require('./Category');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
-  through: {
-    model:Product,//double check this logic..product containes category name
-    unique:false //double check//
-  },
-  //Defome an alias for when data is retrieved
-as: 'product_category'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
+
+  //has just a foreign key
+
+//   through: {
+//     model:Product,//double check this logic..product containes category name
+//     unique:false //double check//
+//   },
+//   //Defome an alias for when data is retrieved
+// as: 'product_category'
+// });
 
 // Categories have many Products
 
 Category.hasMany(Product, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: Product,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'Category_ManyProduct'
+  foreignKey: 'category_id',
 });
+  // Define the third table needed to store the foreign keys
+//   through: {
+//     model: Product,
+//     unique: false
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'Category_ManyProduct'
+// });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'Product_Tag'
+through: ProductTag,
+foreignKey: 'product_id',
 });
+
+//   through: {
+//     model: ProductTag,
+//     unique: false
+//     ///add foreign key 
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'Product_Tag'
+// });
 
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'Tag_ManyProduct'
-});
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  });
+  //   through: {
+//     model: ProductTag,
+//     unique: false
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'Tag_ManyProduct'
+// });
 
 
 module.exports = {
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -39,8 +39,7 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...              body parsing middleware?
-                                                  app.use(bodyParser.text({type 'text/html/}))
+  /* req.body should look like this...             
     {
       product_name: "Basketball",
       price: 200.00,
@@ -48,23 +47,6 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
- //or this way...
-
-  //   Product.create({
-  //     product_name: "Basketball",
-  //     price: "200,000",
-  //    stock: 3,
-  //    tagIds: [1,2,3,4]
-  //   })
-  //     .then((newProduct) => {
-  //       // Send the newly created row as a JSON object
-  //       res.json(newProduct);
-  //     })
-  //     .catch((err) => {
-  //       res.json(err);
-  //     });
-  // });
-    
 
     router.post('/', async (req, res) => {
       try {
@@ -142,6 +124,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
